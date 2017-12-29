fbbf3ce7bd41ffdbb294b93a5801433a314b2881
xuanyuanxixi <1020301980@qq.com>
2017-12-29 13:01:16 +0800

    Initial commit

---
+++ app/src/main/java/com/github/wangfeixixi/myapplication/MainActivity.java
@@ -0,0 +1,12 @@
+package com.github.wangfeixixi.myapplication;
+
+import wangfei.scan2.SampleActivity;
+
+public class MainActivity extends SampleActivity {
+
+
+    @Override
+    public void handleResult(String text) {
+
+    }
+}
---
+++ scan2/src/main/java/wangfei/scan2/SampleActivity.java
@@ -0,0 +1,52 @@
+package wangfei.scan2;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.view.SurfaceView;
+
+import wangfei.scan2.client.android.AutoScannerView;
+import wangfei.scan2.client.android.BaseCaptureActivity;
+import wangfei.scan2.globla.Result;
+
+public abstract class SampleActivity extends BaseCaptureActivity {
+
+    private SurfaceView surfaceView;
+    private AutoScannerView autoScannerView;
+    private Activity mCtx;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr);
+        surfaceView = (SurfaceView) findViewById(R.id.preview_view);
+        autoScannerView = (AutoScannerView) findViewById(R.id.autoscanner_view);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        autoScannerView.setCameraManager(cameraManager);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public SurfaceView getSurfaceView() {
+        return (surfaceView == null) ? (SurfaceView) findViewById(R.id.preview_view) : surfaceView;
+    }
+
+    @Override
+    public void dealDecode(Result rawResult, Bitmap barcode, float scaleFactor) {
+        handleResult(rawResult.getText());
+    }
+
+    public abstract void handleResult(String text);
+}
